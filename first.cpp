f7f00f0e983f04d1133ddfdd7f3c636b314889c0
chiragongit <chirag.dhanerwal.ug22@nsut.ac.in>
2024-08-04 00:37:53 +0530

    linked list

--- first.cpp
+++ first.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    int std[2][4]={
-        {1, 3 ,4, 5},{32, 4 , 23, 5}
-    };
-    for(int i=0; i<2; i++){
-        for (int j = 0; j < 4; j++)
-        {
-            cout<<"the output of "<<i<<","<<j<<" is "<<std[i][j]<<endl;
-        }
-        
+class student{
+private:
+    int marks;
+public:
+    string name;
+    string sclass;
+    string rollno;
+
+    void changename(string newname){
+        name= newname;
     }
-    
+};
+
+int main(){
+    student s1;
+    s1.name="chirag";
+    s1.rollno="2022UEI2819";
+    s1.sclass="2+";;
+    int a;
+    cout<<"enter value to be out"<<endl;
+    s1.changename("radhe");
+
+    // cin>>a;
+    cout<<s1.name<<endl<<s1.rollno<<endl;
+
+    return 0;
+
 
 }
---
+++ linked_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// class node
+// {
+//     public:
+//     int data;
+//     node* next;
+
+
+// }
+int main(){
+    std::string a="chirag";
+    cout<<a;
+    return 0;
+}
