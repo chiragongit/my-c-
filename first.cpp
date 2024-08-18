596d465a0a653a374837d7393d3e68ac3e127916
chiragongit <chirag.dhanerwal.ug22@nsut.ac.in>
2024-08-18 23:00:59 +0530

    all files in first.cpp

--- first.cpp
+++ first.cpp
@@ -3,6 +3,56 @@
 #include <climits>
 #include <utility>
 using namespace std;
+// int findDuplicate(vector<int> &arr){
+//     for(int i=0; i<arr.size();i++){
+//         int unit=arr[i];
+//         for (int j = i+1; j < arr.size(); j++)
+//         {
+//             if(unit==arr[j]){
+//                 return arr[j];
+//             }
+//         }  
+//     }
+//     return 0;
+// }
+// int FindUnique(int arr[], int size){
+//     int ans= 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         ans=ans^arr[i]; 
+//     }
+//     return ans;
+// }
+// int printarr(int arr[], int size){
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }   
+// }
+// bool search(int arr[],int size,int key){
+//     for(int i=0; i<size; i++){
+//         if(arr[i]==key){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// void swap(int& one,int& two){
+//     int a=one;
+//     one=two;
+//     two=a;
+// }
+// void flip(int arr[], int size){
+//     int start=0;
+//     int end=size-1;
+//     for(int i=0;i<size/2;i++)
+//     {
+//         swap(arr[start],arr[end]);
+//         start=start+1;
+//         end=end-1;
+//     }  
+// }
+
 
 int binarySearch(int arr[], int size,int key){
     int start=0;
